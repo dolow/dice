c360c5435caabe58d3bcc06f70adb13c9505a32a
dolow <velvet.nanashi@gmail.com>
2021-02-22 09:48:16 +0900

    added clock, changed anti aliasing level

---
+++ Assets/Scripts/Roles/Clock.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class Clock : MonoBehaviour
+{
+    private float elapsedTime = 0.0f;
+
+    private void Update()
+    {
+        this.elapsedTime += Time.deltaTime;
+        int totalSec = (int)Mathf.Floor(this.elapsedTime);
+        int min = (int)Mathf.Floor(totalSec / 60);
+        int sec = totalSec % 60;
+        Text text = this.GetComponent<Text>();
+        text.text = ("" + min).PadLeft(2, '0') + ":" + ("" + sec).PadLeft(2, '0');
+    }
+}
